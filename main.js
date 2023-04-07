92628a54e3f69292c16c3fd80fc7a2a288b1d163
Michael Dalva <micdal06@gmail.com>
2023-04-07 13:36:36 -0700

    so this has a second console statement

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ console.log('hello!')
 console.log("this is a test, i hope this works");
 
 console.log("I'm GONNA TRY COMMITTING THIS TO MAIN MAHAHAHAH")
+
+console.log("I'm GONNA TRY COMMITTING THIS TO MAIN WITH A SECOND MESSAGE !")
